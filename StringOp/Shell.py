6653bba03e5fe2adcea0cdab838a345afe392286
creepermodeste <slickFaan@hotmail.com>
2012-07-26 15:39:48 +0200

    Ajout des racourcis dans l'aide.

--- StringOp/Shell.py
+++ StringOp/Shell.py
@@ -14,11 +14,13 @@ while True:
                  "starts": strop.starts_with,
                  "ends": strop.ends_with,
                  "inside": strop.word_inside,
+                 "croises": strop.mots_croises,
                  "correction": strop.correction}
     dict_func["e"] = dict_func["ends"]
     dict_func["s"] = dict_func["starts"]
     dict_func["i"] = dict_func["inside"]
     dict_func["c"] = dict_func["correction"]
+    dict_func["cr"] = dict_func["croises"]
     if choice in dict_func.keys():
         arg = input("Chaîne à rechercher : ")
         list_words = dict_func[choice](arg)
--- StringOp/Shell_functions.py
+++ StringOp/Shell_functions.py
@@ -9,14 +9,14 @@ def print_words(list_words):
     
 def print_help():
     print("Liste des commandes disponibles :")
-    print("starts : affiche tous les mots \
+    print("starts (ou s): affiche tous les mots \
 commençant une chaîne.")
-    print("ends : affiche tous les mots finnissant par une chaîne.")
-    print("inside : affiche tous les contenant une chaîne.")
-    print("croises : resolveur de mots croisés( remplacer les\
+    print("ends (ou e): affiche tous les mots finnissant par une chaîne.")
+    print("inside (ou i): affiche tous les contenant une chaîne.")
+    print("croises (ou cr): resolveur de mots croisés( remplacer les\
  lettres que vous ne savez pas par des astérisques. Ex : chat -> c**t).")
-    print("palindromes : cherche tous les palindrome du dictionnaire.")
-    print("correction : rentrez un mot et il revoie tous les mots correspondant avec\
+    print("palindromes (ou p): cherche tous les palindrome du dictionnaire.")
+    print("correction (ou c): rentrez un mot et il revoie tous les mots correspondant avec\
     une fautes de frape.")
     
 
